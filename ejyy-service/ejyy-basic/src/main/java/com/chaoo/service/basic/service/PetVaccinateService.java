c378db58dbdb17c24f84e7729969b297a1303a41
sanshisi <chao2286@163.com>
2022-08-16 10:01:13 +0800

    宠物疫苗

---
+++ ejyy-service/ejyy-basic/src/main/java/com/chaoo/service/basic/service/PetVaccinateService.java
@@ -0,0 +1,8 @@
+package com.chaoo.service.basic.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.chaoo.service.basic.entity.PetVaccinate;
+
+public interface PetVaccinateService extends IService<PetVaccinate> {
+
+}
---
+++ ejyy-service/ejyy-basic/src/main/java/com/chaoo/service/basic/service/impl/PetVaccinateServiceImpl.java
@@ -0,0 +1,12 @@
+package com.chaoo.service.basic.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.chaoo.service.basic.entity.PetVaccinate;
+import com.chaoo.service.basic.mapper.PetVaccinateMapper;
+import com.chaoo.service.basic.service.PetVaccinateService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PetVaccinateServiceImpl extends ServiceImpl<PetVaccinateMapper, PetVaccinate> implements PetVaccinateService {
+
+}
