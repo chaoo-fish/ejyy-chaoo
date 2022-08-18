2f40a07ab2aaf232699c81f735ef7967d5468ab5
sanshisi <chao2286@163.com>
2022-08-18 16:54:24 +0800

    feign初步调试

--- ejyy-service/ejyy-basic/src/main/java/com/chaoo/service/basic/entity/Complain.java
+++ ejyy-service/ejyy-basic/src/main/java/com/chaoo/service/basic/entity/Complain.java
@@ -1,6 +1,7 @@
 package com.chaoo.service.basic.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
--- ejyy-service/ejyy-user/src/main/java/com/chaoo/service/user/dto/UserListInfo.java
+++ ejyy-service/ejyy-user/src/main/java/com/chaoo/service/user/dto/UserListInfo.java
@@ -2,12 +2,14 @@ package com.chaoo.service.user.dto;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * @Author chaoo
  * @Date: 2022/08/17/ 18:45
  */
 @Data
-public class UserListInfo {
+public class UserListInfo implements Serializable {
 
     private String department;
     private Long department_id;
